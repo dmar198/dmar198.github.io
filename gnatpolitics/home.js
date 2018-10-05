6fdc013c081ea0104a556ab044e14212bca47d69
dmar198 <dmar198@gmail.com>
2018-10-04 20:33:09 -0400

    Update home.js

--- gnatpolitics/home.js
+++ gnatpolitics/home.js
@@ -1,1 +1,1 @@
-document.write("Hello");
+document.write("<\?xml version=\"1.0\" encoding=\"UTF-8\" \?><html><head><\/head><body><div style=\"max-width: 600px;margin: auto;border: solid 1px #000000;\">Hello<\/div><\/body><\/html>");
